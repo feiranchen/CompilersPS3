87c6088306716607b5af8d99efe53e16d3a1772f
yinglei <yw437@cornell.edu>
2013-10-02 20:41:11 -0400

    modified ClassCtxt.java

--- src/ClassCtxt.java
+++ src/ClassCtxt.java
@@ -4,23 +4,18 @@ import java.util.List;
 //{$c = new ClassCtxt();} (COMMA k=(INTERFACE | CLASS) CLSINTF p=kindcontext {$c.add($k.text, $CLSINTF.text, $p.kc);}  (EXTENDS t=type {$c.add($t.t);} LBRACE (VAR ts=typescheme SEMICOLON {$c.add($VAR.text, $ts.ts);})* RBRACE)? {$c.finish($k.text);})*;
 
 public class ClassCtxt extends CuClassC {
-	String clsintf, keyword;
-	List<Cls> clsCtxt = new ArrayList<Cls>();
-	Cls curCls;
-	List<Intf> intfCtxt = new ArrayList<Intf>();
-	Intf curIntf;
+	//class or interface
+	List<Ele> elements = new ArrayList<Ele>();
+	
+	String clsintf, keyword;	
 	List<String> kc;
 	CuType t;
 	List<String> VvTypeScheme = new ArrayList<String>(); // temporal
 	public ClassCtxt() {}
 	
 	@Override public void add(String k, String ci, List<String> kc) {
-		if (k=="interface") {
-			//to be done
-		}
-		else {
-			curCls = new Cls(ci, kc, null);
-		}
+		elements.add(new Ele(k, ci, kc));
+		
 		this.keyword = k;
 		this.clsintf = ci;
 		this.kc = kc;
@@ -28,28 +23,49 @@ public class ClassCtxt extends CuClassC {
 	}
 	
 	@Override public void add (CuType t) {
-		//I think it is ok to mix it here
-		curCls.add(t);
-		curIntf.add(t);
+		Ele cur = elements.get(elements.size()-1);
+		cur.set_superType(t);
+		elements.set(elements.size()-1, cur);
+		
 		this.t = t;
 		text += " " +t.toString();
 	}
 	
 	@Override public void add (CuVvc vv, CuTypeScheme ts) {
-		curCls.add(vv, ts, null);
+		Ele cur = elements.get(elements.size()-1);
+		cur.func_add(vv, ts);
+		elements.set(elements.size()-1, cur);
+		
 		String s = String.format("%s %s ;", vv.toString(), ts.toString());
 		VvTypeScheme.add(s);
 		text +=  " " + CuMethod.printList("{", VvTypeScheme, "}", ";");
 	}
 	
-	//add the current object
-	@Override public void finish (String id) {
-		if (id == "interface") {
-			intfCtxt.add(curIntf);
-		}
-		else {
-			clsCtxt.add(curCls);
-		}
+}
+
+class Ele {
+	String keyword;
+	//class/interface names
+	String name;
+	//kind contexts
+	List<String> kc;
+	CuType superType;
+	CuFunC func;
+	
+	Ele(String keyword, String name, List<String> kc) {
+		this.keyword = keyword;
+		this.name = name;
+		this.kc = kc;
+		//default supper type is Top
+		this.superType = new VTopBot("Thing");
+		this.func = new FuncTxt();
 	}
 	
+	public void set_superType(CuType superType) {
+		this.superType = superType;
+	}
+	
+	public void func_add(CuVvc vv, CuTypeScheme ts) {
+		this.func.add(vv, ts);
+	}
 }
